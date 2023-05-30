34bbfabaeea209232599185e4869a4e80e5c6de0
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-05-30 10:54:56 +0000

    Add array of questions, options and correct answers

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const quizContent = [
+    {
+        question: "What is a group of lions called?",
+        a: "A flock",
+        b: "A pride",
+        c: "A herd",
+        correct: "b",
+    },
+    {
+        question: "Which bird is sometimes called a flying rat?",
+        a: "A sparrow",
+        b: "A pigeon",
+        c: "A gull",
+        correct: "b",
+    },
+    {
+        question: "Which animal recognizes itself in the mirror?",
+        a: "A cat",
+        b: "A horse",
+        c: "A chimpanzee",
+        correct: "c",
+    },
+    
+];
